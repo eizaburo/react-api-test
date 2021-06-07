8ebf9c1fdcc383e0326d0c836c8c8ea2625f8ca3
Eizaburo Tamaki <eizaburo.tamaki@gmail.com>
2021-06-07 15:35:26 +0900

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  //state
+  const [state, setStete] = useState({
+    name: "default",
+    message: "...",
+  });
+
+  //change
+  const handleChange = e => {
+    setStete({ ...state, [e.target.name]: e.target.value });
+  }
+
+  //submit
+  const handleSubmit = e => {
+    e.preventDefault();
+    alert(state.name);
+  }
+
+  //api call
+  const handleClick = async () => {
+    //Azure Functionsをcall（ローカルではCORS対応必要：package.jsonにproxyする）
+    const result = await axios.get(`/api/getData?name=${state.name}`);
+    setStete({ ...state, message: result.data })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 30 }}>
+      <h1>Form1</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <input type="submit" value="push" />
+      </form>
+      <button onClick={handleClick}>api call</button>
+      <p>message: {state.message}</p>
     </div>
   );
 }
